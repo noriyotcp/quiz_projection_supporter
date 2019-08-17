c96f7c5b42c3967c8c9d01efcb8b081ba8ba1816
noriyotcp <noriyo.akita@gmail.com>
2019-08-18 07:53:54 +0900

    Upgrade packages
    yarn upgrade-interactive --latest
    
    npx babel-upgrade --write
    https://babeljs.io/blog/2018/07/27/removing-babels-stage-presets
    https://qiita.com/tathuhi10/items/c908f890939797e6bcf3#babeったらerror-pluginpreset-files-are-not-allowed-to-export-objects-only-functions
    
    Add eslint-plugin-vue
    https://dev.to/ferasdawod/setting-up-eslint-for-a-vuejs-project-using-vs-code-i54
    https://vuejs.github.io/eslint-plugin-vue/user-guide/#faq

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    parser: "babel-eslint",
+    sourceType: "module"
   },
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
+  extends: ["standard", "plugin:vue/recommended"],
   globals: {
     __static: true
   },
-  plugins: [
-    'html'
-  ],
-  'rules': {
+  plugins: ["vue"],
+  rules: {
     // allow paren-less arrow functions
-    'arrow-parens': 0,
+    "arrow-parens": 0,
     // allow async-await
-    'generator-star-spacing': 0,
+    "generator-star-spacing": 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0
   }
 }
