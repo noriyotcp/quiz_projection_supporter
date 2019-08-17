ae37ac4d3de6b087e45445a069524c57a9828b9f
noriyotcp <noriyo.akita@gmail.com>
2019-08-18 08:06:17 +0900

    Lint files

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view />
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'quiz_projection_supporter'
-  }
+export default {
+  name: 'QuizProjectionSupporter'
+}
 </script>
 
 <style>
