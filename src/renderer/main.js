b8c43b6e7de5943a1965f17a384b0d8357c271ee
noriyotcp <noriyo.akita@gmail.com>
2019-08-18 20:43:56 +0900

    Introduce bootstrap-vue and bootstrap-honoka

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,6 +3,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap-honoka/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
