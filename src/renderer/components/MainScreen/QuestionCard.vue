389a6b18563bf4f008c4923bdb64208458247380
noriyotcp <noriyo.akita@gmail.com>
2019-08-18 21:44:54 +0900

    Add MainScreen/QuestionCard.vue

---
+++ src/renderer/components/MainScreen/QuestionCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-card
+      :title="title"
+      :sub-title="qIdLabel"
+      class="qCard"
+    >
+      <p class="card-text">
+        {{ qAnswerLabel }}
+      </p>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      qIdLabel: '---',
+      qTextLabel: '---',
+      qAnswerLabel: '---'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.qCard {
+  height: 200px;
+}
+</style>
